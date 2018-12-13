4386b423219c45d5c894090dc4e2ee62e441bc0d
Rohil Tuli <rohil.tuli@gmail.com>
2018-12-13 12:51:22 -0500

    Day 3 completed

---
+++ day3.py
@@ -0,0 +1,33 @@
+file = open("day3.txt")
+map = {}
+for line in file:
+    idSplit = line.split(" @ ")
+    dimensionAndPositionSplit = idSplit[1].split(": ")
+    positionSplit = dimensionAndPositionSplit[0].split(",")
+    dimensionsSplit = dimensionAndPositionSplit[1].split("x")
+    for i in range(int(positionSplit[0]), int(positionSplit[0]) + int(dimensionsSplit[0])):
+        for j in range(int(positionSplit[1]), int(positionSplit[1]) + int(dimensionsSplit[1])):
+            if (i, j) in map:
+                map[(i, j)] = True
+            else:
+                map[(i, j)] = False
+file.close()
+file = open("day3.txt")
+for line in file:
+    idSplit = line.split(" @ ")
+    dimensionAndPositionSplit = idSplit[1].split(": ")
+    positionSplit = dimensionAndPositionSplit[0].split(",")
+    dimensionsSplit = dimensionAndPositionSplit[1].split("x")
+    intact = True
+    for i in range(int(positionSplit[0]), int(positionSplit[0]) + int(dimensionsSplit[0])):
+        for j in range(int(positionSplit[1]), int(positionSplit[1]) + int(dimensionsSplit[1])):
+            if map[(i, j)]:
+                    intact = False
+    if intact:
+        print(idSplit[0][1:]) # Part B Solution
+file.close()
+count = 0
+for tuple in map:
+    if map[tuple]:
+        count += 1
+print(count) # Part A Solution
