0c3c3eee9d4f0bc01dc71b589d12905f2aed6e16
Rohil Tuli <rohil.tuli@gmail.com>
2018-12-16 23:26:34 -0500

    Attempt at Day 5, Part B

--- day5.py
+++ day5.py
@@ -9,13 +9,22 @@ def removeIfNeeded(list):
         list.append(secondLastItem)
         list.append(lastItem)
 file = open("input/day5.txt")
+lettersList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 letters = file.read()
 polymer = []
 count = 0
 for letter in letters:
     polymer.append(letter)
     removeIfNeeded(polymer)
-    if count > 100:
-        break
 print(len(polymer)) # Part A Solution, but 1 higher than actual answer (\n or null terminator)?
 file.close()
+minLength = 100000000
+for x in lettersList:
+    polymer = []
+    for y in letters:
+        if y != x.lower() and y != x.upper():
+            polymer.append(y)
+            removeIfNeeded(polymer)
+    if len(polymer) < minLength:
+        minLength = len(polymer)
+print(minLength) # Part B Solution?
