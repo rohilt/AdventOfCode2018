d9679f7da335d6155c81ac8b4d44f4e6ba4ff491
Rohil Tuli <rohil.tuli@gmail.com>
2018-12-19 18:17:02 -0500

    Trying to visualize Day 6

--- day6.py
+++ day6.py
@@ -3,4 +3,10 @@ tuples = []
 for line in file:
     pair = line.split(", ")
     tuples.append((int(pair[0]), int(pair[1])))
-print(tuples)
+for x in range(0, 400):
+    for y in range(0, 400):
+        if (x, y) in tuples:
+            print("X", end="")
+        else:
+            print("O", end="")
+    print("")
