f3ed342db19a6742b120324806efd8e48ad25757
Rohil Tuli <rohil.tuli@gmail.com>
2018-12-22 23:15:15 -0500

    Processed input for Day 7

--- input/day7.py
+++ input/day7.py
@@ -1,3 +1,9 @@
 file = open("input/day7.txt")
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+prereqs = {}
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+for letter in alphabet:
+    prereqs[letter] = []
 for line in file:
+    split = line.split(" ")
+    prereqs[split[7]].append(split[1])
+print(prereqs)
